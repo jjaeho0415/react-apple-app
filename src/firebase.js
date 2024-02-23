547309d30284ae35a601cc5e67e01dd4c9dc00cd
jjaeho0415 <jjh0773@naver.com>
2024-02-23 17:26:59 +0900

    chapter6 finished

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA8vKXgg99M9p7GkOJ7uNlZklARkLtSf_A",
+  authDomain: "react-apple-tv-1eeac.firebaseapp.com",
+  projectId: "react-apple-tv-1eeac",
+  storageBucket: "react-apple-tv-1eeac.appspot.com",
+  messagingSenderId: "378403834157",
+  appId: "1:378403834157:web:c904cbb90c8ab788f36ba4",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
